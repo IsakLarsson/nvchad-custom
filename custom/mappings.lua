d4062ec90a1a91d4f663b01592244a8aa80c5a7a
Isak Larsson <isaklarsson98@hotmail.com>
2023-05-27 11:26:39 +0200

    terminal toggle mappings

--- custom/mappings.lua
+++ custom/mappings.lua
@@ -30,13 +30,45 @@ M.custom = {
 			"<cmd>:UndotreeToggle<CR>",
 			"Toggle undo tree",
 		},
+		["˛"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle horizontal term",
+		},
+
+		["‹"] = {
+			function()
+				require("nvterm.terminal").toggle("vertical")
+			end,
+			"Toggle vertical term",
+		},
 	},
+
 	i = {
 		["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
 	},
+
 	v = {
 		["K"] = { ":m '<-2<CR>gv=gv", "Move selected code up" },
 		["J"] = { ":m '>+1<CR>gv=gv", "Move selected code down" },
+	},
+
+	t = {
+
+		["˛"] = {
+			function()
+				require("nvterm.terminal").toggle("horizontal")
+			end,
+			"Toggle horizontal term",
+		},
+
+		["‹"] = {
+			function()
+				require("nvterm.terminal").toggle("vertical")
+			end,
+			"Toggle vertical term",
+		},
 	},
 }
 
