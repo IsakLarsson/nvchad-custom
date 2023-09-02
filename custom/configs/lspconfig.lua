29091249785826f8231cdc307b4714bc4a88d5cd
Isak Larsson <isaklarsson98@hotmail.com>
2023-09-02 19:17:36 +0200

    fixed elm lsp bug

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -10,3 +10,12 @@ for _, lsp in ipairs(servers) do
 		capabilities = capabilities,
 	})
 end
+
+local custom_attach = function(client)
+  if client.config.flags then
+    client.config.flags.allow_incremental_sync = true
+  end
+end
+lspconfig.elmls.setup({
+   on_attach = custom_attach;
+})
