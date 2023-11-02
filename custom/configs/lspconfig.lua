effd66ebd5e49c60a545f179aa4446bc75c12fbc
Isak Åman Larsson <isak.larsson@savr.com>
2023-11-02 08:37:21 +0100

    add groovyls

--- custom/configs/lspconfig.lua
+++ custom/configs/lspconfig.lua
@@ -2,7 +2,7 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
-local servers = { "rust_analyzer", "tsserver", "tailwindcss", "pyright", "cssls", "elmls", "bashls" }
+local servers = { "rust_analyzer", "tsserver", "tailwindcss", "pyright", "cssls", "elmls", "bashls", "groovyls" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
