a571c71715539149f9a1897a0148d629753334df
Isak Åman Larsson <isak.larsson@savr.com>
2024-05-02 14:02:04 +0200

    add refactoring plugin

--- custom/plugins.lua
+++ custom/plugins.lua
@@ -8,6 +8,14 @@ local masonServers = {
 
 local plugins = {
 	{
+		"ThePrimeagen/refactoring.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function() require("refactoring").setup() end,
+	},
+	{
 		"NvChad/nvterm",
 		config = function()
 			require("nvterm").setup({
